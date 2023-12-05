f3a68c75381b0c55e860c520a6a83c724b705777
RiccardoPollice <riccardo.pollice17@gmail.com>
2023-12-05 15:32:54 +0100

    End work

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,2 +1,19 @@
-package org.lessons.java.geometria;public class Main {
+package org.lessons.java.geometria;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci l'altezza");
+        int altezzaInserita = scanner.nextInt();
+
+        System.out.println("Inserisci la base");
+        int baseInserita = scanner.nextInt();
+        Rettangolo myRectangle = new Rettangolo(altezzaInserita, baseInserita);
+
+        System.out.println("l'area del rettangolo è : " + myRectangle.calcolaArea());
+        System.out.println("il perimetro del rettangolo è : " + myRectangle.calcoloPerimetro());
+
+    }
 }
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,2 +1,23 @@
-package org.lessons.java.geometria;public class Rettangolo {
+package org.lessons.java.geometria;
+
+public class Rettangolo {
+    //Attributi
+    int base;
+    int altezza;
+
+    //Costruttori
+    public Rettangolo(int base, int altezza){
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    //Metodi
+    int calcolaArea(){
+        return base * altezza;
+
+    }
+
+    int calcoloPerimetro(){
+        return (base + altezza) * 2;
+    }
 }
